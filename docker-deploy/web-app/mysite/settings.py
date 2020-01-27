9496c6f6394bbabd98eb80ac7114a0d248db7cf4
Ubuntu <ubuntu@ip-172-31-30-28.us-east-2.compute.internal>
2020-01-27 23:43:33 +0000

    docker-compose up is working and presql connection good

--- docker-deploy/web-app/mysite/settings.py
+++ docker-deploy/web-app/mysite/settings.py
@@ -75,10 +75,23 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'HOST': 'db',
+        'PORT': 5432,
     }
 }
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'prj1_568db',
+#         'USER': 'postgres',
+#         'PASSWORD': 'postgres',
+#         'HOST': '127.0.0.1',
+#         'PORT': 5432,
+#     }
+# }
 
 
 # Password validation
