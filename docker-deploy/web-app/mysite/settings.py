89bf7384ee26c8e201608d32e0cc0a1e6ca6b395
Ubuntu <ubuntu@ip-172-31-30-28.us-east-2.compute.internal>
2020-01-28 00:10:29 +0000

    /uper page showing a single text

--- docker-deploy/web-app/mysite/settings.py
+++ docker-deploy/web-app/mysite/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
+   # 'uper.apps.UperConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
---
+++ docker-deploy/web-app/uper/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class UperConfig(AppConfig):
+    name = 'uper'
