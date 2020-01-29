433f9996df10f27a784ae5bbe313d82fda275697
Ubuntu <ubuntu@ip-172-31-30-28.us-east-2.compute.internal>
2020-01-29 03:13:32 +0000

    owner request ride function complete

--- docker-deploy/web-app/uper/models.py
+++ docker-deploy/web-app/uper/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.postgres.fields import ArrayField
 
 # Create your models here.
 from django.db import models
@@ -7,4 +8,43 @@ from django.db import models
 class User(models.Model):
     username = models.CharField(max_length=20)
     password = models.CharField(max_length=20)
-    
+
+class Ride(models.Model):
+    # the state of the ride can be: open/confirmed/closed/complete
+    state = models.CharField(max_length=10)
+    # id of the ride owner: default 0, cannot be 0
+    # !! should be dynamically allocated as the owner goes away
+    owner_id = models.IntegerField(default=0)
+    # id(s) of the ride sharer(s):
+    #   appended to the existing string's tail in time order
+    #   when owner goes away, the leading sharer will automatically become the new owner
+    #   can be empty
+    #   max 9 sharers
+    sharer_id_list = ArrayField(
+        models.IntegerField(default=0),
+        size = 9,
+        null = True,
+    )
+    # driver id, can be 0, indicating no driver has picked up this ride yet
+    driver_id = models.IntegerField(default=0)
+    # required arrival datetime, can only be set/edited by owner
+    arrival_datetime = models.DateTimeField()
+    # destination, cannot be null, can only be set/edited by owner
+    destination = models.CharField(max_length = 100)
+    # this ride can be shared: cannot be null, can only be set/edited by owner
+    # yes or no
+    can_share = models.CharField(max_length = 10)
+    # current person total number: 10 slots, first slot for owner, 1-1 relationship sharer
+    person_number_list = ArrayField(
+        models.IntegerField(default=0),
+        size = 10,
+        null = True, # default is null
+    )
+    # other info: a string, can only be set/edited by owner
+    # matching: strictly
+    # !! optional
+    other_info = models.CharField(max_length=200)
+    # required vehicle type, can only be set/edited by sharer
+    # matching: strictly
+    # !! optional
+    required_vehicle_type = models.CharField(max_length=200)
--- docker-deploy/web-app/uper/urls.py
+++ docker-deploy/web-app/uper/urls.py
@@ -9,5 +9,7 @@ urlpatterns = [
     path('register_process/', views.register_process, name='register_process'),
     path('login/', views.login, name='login'),
     path('main_page/', views.main_page, name='main_page'),
+    path('request_ride/', TemplateView.as_view(template_name="uper/request_ride.html"), name='request_ride'),
+    path('request_ride_process/', views.request_ride_process, name="request_ride_process"),
 ]
 
